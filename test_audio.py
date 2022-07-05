3d7491a681231e78941f8bab59d20fa4f85e191d
COHEN Jeremy <jeremy.cohen@cnrs.fr>
2022-07-05 17:02:34 +0200

    shootout for audio

--- test_audio.py
+++ test_audio.py
@@ -9,6 +9,10 @@ import utils
 from tensorly.tenalg.proximal import fista
 import soundfile as sf
 from scipy import signal
+# personal toolbox
+from shootout.methods.runners import run_and_track
+from shootout.methods.post_processors import find_best_at_all_thresh, df_to_convergence_df, error_at_time_or_it
+from shootout.methods.plotters import plot_speed_comparison
 '''
  W is a dictionary of 88 columns and 4097 frequency bins. Each column was obtained by performing a rank-one NMF (todo correct) on the recording of a single note in the MAPS database, on a Yamaha Disklavier with close microphones, the note was played mezzo forte and the loss was beta-divergence with beta=1.
 
@@ -77,83 +81,61 @@ df = pd.DataFrame()
 
 Wgt = Wgt[:,:rank]
 Hgt = Hgt[:rank,:]
+algs = ["Lee_Sung_l2", "Proposed_l2", "GD_l2", "NeNMF_l2", "HALS_l2"]
 
-for s in range(Nb_seeds):
-
-    # print
-    print("Loop number is", s)
-
+@run_and_track(
+    nb_seeds=Nb_seeds,
+    algorithm_names=algs, 
+)
+def one_run(rank = 88,
+            tol = 0,
+            NbIter = 500,
+            NbIter_hals = 150,
+            NbIter_inner = 10,
+            pert_sigma = 0.1,
+            epsilon = 1e-8):
     # Perturbing the initialization for randomization
     Wini = use_gt*Wgt + pert_sigma*np.random.rand(m, rank)
     Hini = use_gt*Hgt + pert_sigma*np.random.rand(rank, n)
 
-    print('Running Lee and Seung NMF')
     error0, W0, H0, toc0 = nmf_f.NMF_Lee_Seung(Y,  Wini, Hini, NbIter, NbIter_inner,tol=tol, legacy=False, epsilon=epsilon)
-    print('Running our proposed NeNMF')
     error1, W1, H1, toc1  = nmf_f.NeNMF_optimMajo(Y, Wini, Hini, tol=tol, itermax=NbIter, nb_inner=NbIter_inner, epsilon=epsilon)
-    print('Running proposed MU')
-    # todo
-    #error5, W5, H5, toc5 = nmf_f.NMF_proposed_Frobenius(Y, Wini, Hini, NbIter, NbIter_inner, tol=tol)
-    print('Running Proximal Gradient Descent')
     error2, W2, H2, toc2  = nmf_f.Grad_descent(Y , Wini, Hini, NbIter, NbIter_inner, tol=tol, epsilon=epsilon)
-    print('Running NeNMF')
     error3, W3, H3, toc3  = nmf_f.NeNMF(Y, Wini, Hini, tol=tol, nb_inner=NbIter_inner, itermax=NbIter, epsilon=epsilon)
-    # Fewer max iter cause too slow
-    print('Running HALS')
     W4, H4, error4, toc4 = nn_fac.nmf.nmf(Y, rank, init="custom", U_0=np.copy(Wini), V_0=np.copy(Hini), n_iter_max=NbIter_hals, tol=tol, update_rule='hals',beta=2, return_costs=True, NbIter_inner=NbIter_inner)
-    
-    dic= {
-            "batch_size": 5, # number of algorithms in each comparison run
-            "method": ["NMF_LeeSeung (modern)",  "NeNMF_optimMajorant", "PGD", "NeNMF", "HALS"],
-            "m": m,
-            "n": n,
-            "r": rank,
-            "seed_idx": s,
-            "use_gt": use_gt,
-            "epsilon": epsilon,
-            "pert_sigma": pert_sigma,
-            "NbIter": NbIter,
-            "NbIter_inner": NbIter_inner,
-            "NbIter_hals": NbIter_hals,
-            "tol": tol,
-            "final_error": [error0[-1], error1[-1], error2[-1], error3[-1], error4[-1]],
-            "total_time": [toc0[-1], toc1[-1], toc2[-1], toc3[-1], toc4[-1]],
-            "full_error": [error0,error1,error2,error3,error4],
-            "full_time": [toc0,toc1,toc2,toc3,toc4],
-            "NbIterStop": [len(error0),len(error1),len(error2),len(error3),len(error4)]
-        }
 
-    df = pd.concat([df,pd.DataFrame(dic)], ignore_index=True)
+    return {"errors": [error0, error1, error2, error3, error4], "timings": [toc0,toc1,toc2,toc3,toc4]}
+    
 
 # Winner at given threshold plots
-min_thresh = np.log10(error0[0])
-max_thresh = np.log10(error1[-1])
-thresh = np.logspace(min_thresh,max_thresh-1,50)
-scores_time, scores_it, timings, iterations = utils.find_best_at_all_thresh(df,thresh, 5)
+#min_thresh = np.log10(error0[0])
+#max_thresh = np.log10(error1[-1])
+#thresh = np.logspace(min_thresh,max_thresh-1,50)
+#scores_time, scores_it, timings, iterations = utils.find_best_at_all_thresh(df,thresh, 5)
 
-fig0 = plt.figure()
-plt.subplot(121)
-plt.semilogx(thresh, scores_time.T)
-plt.legend(["NMF_LeeSeung",  "NeNMF_optimMajorant", "PGD", "NeNMF", "HALS"])
-plt.title('How many times each algorithm reached threshold the fastest (time)')
-plt.xlabel('Rec error threshold')
-plt.ylabel('Number of faster runs')
-plt.subplot(122)
-plt.semilogx(thresh, scores_it.T)
-plt.legend(["NMF_LeeSeung",  "NeNMF_optimMajorant", "PGD", "NeNMF", "HALS"])
-plt.title('How many times each algorithm reached threshold the fastest (iters)')
-plt.xlabel('Rec error threshold')
-plt.ylabel('Number of faster runs')
+#fig0 = plt.figure()
+#plt.subplot(121)
+#plt.semilogx(thresh, scores_time.T)
+#plt.legend(["NMF_LeeSeung",  "NeNMF_optimMajorant", "PGD", "NeNMF", "HALS"])
+#plt.title('How many times each algorithm reached threshold the fastest (time)')
+#plt.xlabel('Rec error threshold')
+#plt.ylabel('Number of faster runs')
+#plt.subplot(122)
+#plt.semilogx(thresh, scores_it.T)
+#plt.legend(["NMF_LeeSeung",  "NeNMF_optimMajorant", "PGD", "NeNMF", "HALS"])
+#plt.title('How many times each algorithm reached threshold the fastest (iters)')
+#plt.xlabel('Rec error threshold')
+#plt.ylabel('Number of faster runs')
 
-# Error plots
-fig_convergence_plots = plt.figure()
-plt.semilogy(toc0,error0, label="Lee Seung NMF")
-plt.semilogy(toc1,error1, label="Phan NeNMF")
-plt.semilogy(toc2,error2, label="PGD")
-plt.semilogy(toc3,error3, label="NeNMF")
-plt.semilogy(toc4,error4, label="HALS")
-plt.legend(fontsize = 14)
-plt.show()
+## Error plots
+#fig_convergence_plots = plt.figure()
+#plt.semilogy(toc0,error0, label="Lee Seung NMF")
+#plt.semilogy(toc1,error1, label="Phan NeNMF")
+#plt.semilogy(toc2,error2, label="PGD")
+#plt.semilogy(toc3,error3, label="NeNMF")
+#plt.semilogy(toc4,error4, label="HALS")
+#plt.legend(fontsize = 14)
+#plt.show()
 
 
 #-----------------------------------------------------------------
