df8ba6a3268cc287e1f7fda24243cc79abacca23
COHEN Jeremy <jeremy.cohen@cnrs.fr>
2022-07-05 16:21:59 +0200

    changing noise

--- synthetic_comparisons_KL.py
+++ synthetic_comparisons_KL.py
@@ -17,16 +17,17 @@ plt.close('all')
 # --------------------- Choose parameters for grid tests ------------ #
 algs = ["Lee_Sung", "Fevotte_Idier", "Proposed"]
 nb_seeds = 3
-@run_and_track(algorithm_names=algs, path_store="Results/", name_store="KL_run_test",
+@run_and_track(algorithm_names=algs, path_store="Results/", name_store="KL_run_unif_noise",
                 nb_seeds=nb_seeds, # Change this to >0 to run experiments
                 m = [50,100],
                 n = [50,100],
                 r = [5,10],
-                sigma = [0,1e-7,1e-4],
+                sigma = [0,1e-2,1],
                 NbIter_inner = [2,10]
                 )
-def one_run(m=100,n=100,r=10,sigma=0, NbIter=3000,tol=0,NbIter_inner=10, verbose=True):
+def one_run(m=100,n=100,r=10,sigma=0, NbIter=3000,tol=0,NbIter_inner=10, verbose=True, show_it=1000):
     # Fixed the signal 
+    # here it is dense, TODO: try sparse
     Worig = np.random.rand(m, r) 
     Horig = np.random.rand(r, n)  
     Vorig = Worig.dot(Horig)
@@ -36,24 +37,27 @@ def one_run(m=100,n=100,r=10,sigma=0, NbIter=3000,tol=0,NbIter_inner=10, verbose
     Wini = np.random.rand(m, r) #sparse.random(rV, cW, density=0.25).toarray() 
     
     # adding Poisson noise to the observed data
-    N = np.random.poisson(sigma,size=Vorig.shape) # integers, should we scale?
+    # TODO: discuss noise choice
+    #N = np.random.poisson(sigma,size=Vorig.shape) # integers, should we scale?
+    N = sigma*np.random.rand(m,n) # uniform, should we scale?
     V = Vorig + N
 
     # One noise, one init; NMF is not unique and nncvx so we will find several results
-    error0, W0, H0, toc0 = nmf_kl.Lee_Seung_KL(V, Wini, Hini, NbIter=NbIter, nb_inner=NbIter_inner, tol=tol, verbose=verbose)
-    error1, W1, H1, toc1 = nmf_kl.Fevotte_KL(V, Wini, Hini, NbIter=NbIter, nb_inner=NbIter_inner, tol=tol, verbose=verbose)
+    error0, W0, H0, toc0 = nmf_kl.Lee_Seung_KL(V, Wini, Hini, NbIter=NbIter, nb_inner=NbIter_inner, tol=tol, verbose=verbose, print_it=show_it)
+    error1, W1, H1, toc1 = nmf_kl.Fevotte_KL(V, Wini, Hini, NbIter=NbIter, nb_inner=NbIter_inner, tol=tol, verbose=verbose, print_it=show_it)
     #error2, W2, H2, toc2 = nmf_kl.NeNMF_KL(V, Wini, Hini, NbIter=NbIter, nb_inner=NbIter_inner, tol=tol)
-    error3, W3, H3, toc3 = nmf_kl.Proposed_KL(V, Wini, Hini, NbIter=NbIter, nb_inner=NbIter_inner, tol=tol, verbose=verbose)
+    error3, W3, H3, toc3 = nmf_kl.Proposed_KL(V, Wini, Hini, NbIter=NbIter, nb_inner=NbIter_inner, tol=tol, verbose=verbose, print_it=show_it)
 
     return {"errors" : [error0, error1, error3], 
             "timings" : [toc0, toc1, toc3],
-            "noise": [N,N,N]}
+            #"noise": [N,N,N]
+            }
 
 
 # -------------------- Post-Processing ------------------- #
 import pandas as pd
 
-name = "KL_run_test"
+name = "KL_run_unif_noise"
 df = pd.read_pickle("Results/"+name)
 
 # Using shootout for plotting and postprocessing
